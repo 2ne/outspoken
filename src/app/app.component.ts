6141b8a576ca9867bb705012d7fbf4f67e4c7db6
James <jamestoone@gmail.com>
2021-02-27 22:31:28 +0000

    add scrollout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,26 @@
-import {Component, HostListener} from '@angular/core';
+import {AfterContentInit, Component, ElementRef, OnDestroy} from '@angular/core';
+import ScrollOut from 'scroll-out';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit, OnDestroy {
   title = 'OutSpoken';
+  so: any;
 
-  scrolled = false;
+  constructor(private el: ElementRef) {}
 
-  @HostListener('window:scroll', [])
-  onWindowScroll(): void {
-    this.scrolled = window.scrollY > 0;
+  ngAfterContentInit(): void {
+    this.so = ScrollOut({
+      threshold: .25,
+      once: true,
+      scope: this.el.nativeElement
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.so.teardown();
   }
 }
